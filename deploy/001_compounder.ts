945e5e6929443e6efc06e5a7840dbb8ccb357e5a
farreldarian <farreldarian@users.noreply.github.com>
2022-01-15 18:28:09 +0700

    Update deployment

--- deploy/001_compounder.ts
+++ deploy/001_compounder.ts
@@ -5,11 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { ethers, deployments } = hre
 
   const [signer] = await ethers.getSigners()
-  const result = await deployments.deploy('Greeter', {
+  const result = await deployments.deploy('AvalynCompounder', {
     from: signer.address,
-    args: ['Hello, Hardhat!'],
+    log: true,
+    args: [[]],
   })
 
-  console.log('Greeter deployed to:', result.address)
+  console.log('Compounder deployed to:', result.address)
 }
 export default func
+func.tags = ['Compounder']
